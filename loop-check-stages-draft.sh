b78d78368fa4cefcb6186d38bfbfbc0fcdec71e4
maheshopsmx <mahesh.kota@opsmx.io>
2023-04-14 16:45:49 +0530

    check stages

--- loop-check-stages-draft.sh
+++ loop-check-stages-draft.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+git clone https://github.com/maheshopsmx/pipeline-json.git
+cd pipeline-json
+rm -rf /tmp/list
+touch /tmp/list
 stages=waitstage,manualjudgement
 IFS=","
 for liststages in $stages
@@ -9,8 +13,10 @@ do
   do
     if [ "$liststages" = "$checkstages" ]
     then
-       echo "Yes stage matches $stages = $checkstages"
-       break
+       echo "Yes stage matches $liststages = $checkstages"
+       ls | grep $checkstages >> /tmp/list
+       cat /tmp/list
+       exit 0
     else
        echo "Check Matching ...."
     fi
