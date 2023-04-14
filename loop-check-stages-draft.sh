73f23b790a7be7c65ccfad4c4d3f584cbbd60509
maheshopsmx <mahesh.kota@opsmx.io>
2023-04-14 17:26:17 +0530

    loop stages handeled

--- loop-check-stages-draft.sh
+++ loop-check-stages-draft.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-git clone https://github.com/maheshopsmx/pipeline-json.git
+git clone https://github.com/maheshopsmx/pipeline-json.git > /dev/null
 cd pipeline-json
-rm -rf /tmp/list
-touch /tmp/list
-stages=waitstage,manualjudgement
+rm -rf ../list
+touch ../list
+stages=aws,waitstage,manualjudgement
 IFS=","
 for liststages in $stages
 do
@@ -11,14 +11,42 @@ do
   IFS=","
   for checkstages in $existone
   do
-    if [ "$liststages" = "$checkstages" ]
+    if [ "$liststages" == "$checkstages" ]
     then
        echo "Yes stage matches $liststages = $checkstages"
-       ls | grep $checkstages >> /tmp/list
-       cat /tmp/list
-       exit 0
-    else
-       echo "Check Matching ...."
+       ls | grep $checkstages >> ../list
+       if [ "$?" != "0" ]
+       then
+          echo "The stage name:--> $liststages not found .." 
+       fi
+    #else
+    #   echo "Check Matching ...."
     fi
  done
 done
+stageid=
+refid=1
+IFS=","
+while read -r filestg; do
+    echo value is $filestg
+    jq  --argjson stage "$(<$filestg)" '.stages += [$stage]' plain_pipeline_template.json > formulate.json
+    if [[ -z "$stageid" ]]; then
+    jq --argjson refstage '{"refId":"'$refid'","requisiteStageRefIds":['"$stageid"']}' '.stages['"$stageid"'] += $refstage' formulate.json > inter.json
+    else
+    jq --argjson refstage '{"refId":"'$refid'","requisiteStageRefIds":["'"$stageid"'"]}' '.stages['"$stageid"'] += $refstage' formulate.json > inter.json
+    fi
+    if [ $? != 0 ]; then
+      echo "Error occured in YAML processing, please check the logs"
+      exit 1
+    fi
+    rm -rf plain_pipeline_template.json formulate.json final_pipeline.json
+    mv inter.json plain_pipeline_template.json
+    rm -rf inter.json
+    filestg=manualjudgement_stage.json
+    refid=$((refid+1))
+    stageid=$((stageid+1))
+done < ../list
+rm -rf ../list
+mkdir final-pipeline
+cp plain_pipeline_template.json final-pipeline/final_pipeline.json
+cat final-pipeline/final_pipeline.json
